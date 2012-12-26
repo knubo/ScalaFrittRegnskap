37b3f0a469cd869a2b366d041eff64239a9a0a7b
Knut Erik Borgen <keb@conduct.no>
2012-12-26 15:31:12 +0100

    First post with dispatch working

--- src/main/scala/FrittRegnskapCore.scala
+++ src/main/scala/FrittRegnskapCore.scala
@@ -1,15 +1,35 @@
 
-
 import net.noerd.prequel.DatabaseConfig
 import net.noerd.prequel.SQLFormatterImplicits._
 import net.noerd.prequel.ResultSetRowImplicits._
+import dispatch._
 
-class FrittRegnskapCore {
+object FrittRegnskapCore {
 
   val database = DatabaseConfig(
-    driver = "org.mysql.jdbc.JDBCDriver",
-    jdbcURL = "jdbc:mysql://localhost:3306/fr"
-  )
+    driver = "com.mysql.jdbc.Driver",
+    jdbcURL = "jdbc:mysql://localhost:3306/fr",
+    username = "root")
 
+  def addInstall = {
+    database.transaction { tx =>
+      tx.execute("delete from to_install")
+      tx.execute("insert into to_install (secret, wikilogin) values ('test','test')")
+    }
 
+    val u = url("http://master.local.no/RegnskapServer/services/admin/admin_install.php").POST
+    val withParams = u.addParameter("secret", "test").addParameter("wikilogin", "test").addParameter("phone", "98257810").
+      addParameter("zipcode", "1068").addParameter("city", "Oslo").addParameter("address", "Ole Reistads vei 25B").
+      addParameter("email", "knutbo@ifi.uio.no").addParameter("contact", "Knut Erik").addParameter("clubname", "Minklubb").
+      addParameter("domainname", "test").addParameter("password", "passor1").addParameter("superuser", "super")
+
+    val result = Http(withParams OK as.String)
+
+    println("Create domain result:" + result())
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    addInstall
+  }
 }
