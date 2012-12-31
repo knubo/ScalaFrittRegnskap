2c25c68c26e7f06418acc7514aa34dc30562c3e9
Knut Erik Borgen <keb@conduct.no>
2012-12-31 10:22:35 +0100

    Removed use of dispatch going with bee client

--- build.sbt
+++ build.sbt
@@ -12,8 +12,18 @@ libraryDependencies += "org.scalatest" % "scalatest_2.9.0" % "1.6.1"
 
 libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.21"
 
-libraryDependencies += "net.databinder.dispatch" %% "dispatch-core" % "0.9.5"
-
 libraryDependencies +=  "net.noerd" %% "prequel" % "0.3.8" changing()
+
+libraryDependencies ++= Seq(
+    "uk.co.bigbeeconsultants" %% "bee-client" % "0.18.+",
+    "org.slf4j" % "slf4j-api" % "1.7.+",
+    "ch.qos.logback" % "logback-core"    % "1.0.+",
+    "ch.qos.logback" % "logback-classic" % "1.0.+"
+)
+
+resolvers += "Big Bee Consultants" at "http://www.bigbeeconsultants.co.uk/repo"
+
+
+
 
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.1")
--- src/main/scala/FrittRegnskapCore.scala
+++ src/main/scala/FrittRegnskapCore.scala
@@ -2,7 +2,9 @@
 import net.noerd.prequel.DatabaseConfig
 import net.noerd.prequel.SQLFormatterImplicits._
 import net.noerd.prequel.ResultSetRowImplicits._
-import dispatch._
+import uk.co.bigbeeconsultants.http.request.RequestBody
+import uk.co.bigbeeconsultants.http._
+import uk.co.bigbeeconsultants.http.response.Status
 
 object FrittRegnskapCore {
 
@@ -17,19 +19,42 @@ object FrittRegnskapCore {
       tx.execute("insert into to_install (secret, wikilogin) values ('test','test')")
     }
 
-    val u = url("http://master.local.no/RegnskapServer/services/admin/admin_install.php").POST
-    val withParams = u.addParameter("secret", "test").addParameter("wikilogin", "test").addParameter("phone", "98257810").
-      addParameter("zipcode", "1068").addParameter("city", "Oslo").addParameter("address", "Ole Reistads vei 25B").
-      addParameter("email", "knutbo@ifi.uio.no").addParameter("contact", "Knut Erik").addParameter("clubname", "Minklubb").
-      addParameter("domainname", "test").addParameter("password", "passor1").addParameter("superuser", "super")
+    val requestBody = RequestBody(Map("secret" -> "test", "wikilogin" -> "test", "phone" -> "98257810", "zipcode" -> "1068", "city" -> "Oslo", "address" -> "Ole Reistads vei 25B",
+      "email" -> "knutbo@ifi.uio.no", "contact" -> "Knut Erik", "clubname" -> "Minklubb", "domainname" -> "test", "password" -> "passor1", "superuser" -> "super"))
 
-    val result = Http(withParams OK as.String)
+    val url = "http://master.local.no/RegnskapServer/services/admin/admin_install.php"
+    val httpClient = new HttpClient
+    val response = httpClient.post(url, Some(requestBody))
 
-    println("Create domain result:" + result())
+    println("Create domain result:" + response.body)
+
+  }
+
+  def authBySecret = {
+    val secret = (System.currentTimeMillis() + 100000) + "-123"
+    database.transaction { tx =>
+      tx.execute("update master_person set secret = '" + secret + "' where id = (select person from master_user where username = 'knubo')")
+    }
+    val url = "http://master.local.no/RegnskapServer/services/authenticate.php"
+
+    val requestBody = RequestBody(Map("action" -> "secret", "secret" -> secret, "username" -> "knubo"))
+
+    val httpClient = new HttpClient
+    val response = httpClient.post(url, Some(requestBody))
+
+    if (response.status != Status.S200_OK) {
+    	throw new RuntimeException("Did not get forwarded as expected. " + response.status+ " Body was:"+response.body)
+    }
+
+    val checkBody = RequestBody(Map("action" -> "test"))
+    val responseCheck = httpClient.post(url, Some(checkBody))
+
+    assert(responseCheck.body.equals("knubo"))
 
   }
 
   def main(args: Array[String]): Unit = {
-    addInstall
+    // addInstall
+    authBySecret
   }
 }
